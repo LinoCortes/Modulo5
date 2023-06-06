c5cffc28a93d4566ee86ada939a0dd17d6cfe770
Lino Cortés <lino.cortess@gmail.com>
2023-06-06 18:36:17 -0400

    Se corrige identación

--- src/main/java/cl/ejerciciogrupal1/controllers/servlets/ServletCrearCapacitacion.java
+++ src/main/java/cl/ejerciciogrupal1/controllers/servlets/ServletCrearCapacitacion.java
@@ -32,6 +32,7 @@ public class ServletCrearCapacitacion extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
--- src/main/java/cl/ejerciciogrupal1/controllers/servlets/ServletIndex.java
+++ src/main/java/cl/ejerciciogrupal1/controllers/servlets/ServletIndex.java
@@ -42,7 +42,8 @@ public class ServletIndex extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		//request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	/**
